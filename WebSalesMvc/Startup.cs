986c2f5aad9a9deaee86057c090137913f83d4ee
Rubens Lima <rubens.souzalima@hotmail.com>
2024-03-03 17:14:16 -0300

    MySQL adaption and first migration

--- WebSalesMvc/Startup.cs
+++ WebSalesMvc/Startup.cs
@@ -37,7 +37,8 @@ namespace WebSalesMvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebSalesMvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebSalesMvcContext")));
+                    options.UseMySql(Configuration.GetConnectionString("WebSalesMvcContext"), builder =>
+                        builder.MigrationsAssembly("WebSalesMvc")));            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
