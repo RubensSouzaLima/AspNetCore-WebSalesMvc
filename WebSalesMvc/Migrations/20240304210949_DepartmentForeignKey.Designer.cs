9d17f7d63c5324386ca8b296aa3c94b43e013c23
Rubens Lima <rubens.souzalima@hotmail.com>
2024-03-04 18:24:21 -0300

    Foreign Key not null

--- WebSalesMvc/Migrations/20240304210949_DepartmentForeignKey.Designer.cs
+++ WebSalesMvc/Migrations/20240304210949_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebSalesMvc.Data;
 
 namespace WebSalesMvc.Migrations
 {
     [DbContext(typeof(WebSalesMvcContext))]
-    partial class WebSalesMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20240304210949_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- WebSalesMvc/Models/Seller.cs
+++ WebSalesMvc/Models/Seller.cs
@@ -18,6 +18,8 @@ namespace WebSalesMvc.Models
         
         public Department Department { get; set; }
 
+        public int DepartmentId { get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
